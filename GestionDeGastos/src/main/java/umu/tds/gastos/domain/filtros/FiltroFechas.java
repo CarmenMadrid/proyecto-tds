68a0216e3ec4a7bfdb8638f0e6783f3c1b824af1
Alvaro <alvaro.s.g1@um.es>
2026-05-21 20:23:37 +0200

    Filtro de fechas

---
+++ GestionDeGastos/src/main/java/umu/tds/gastos/domain/filtros/FiltroFechas.java
@@ -0,0 +1,36 @@
+package umu.tds.gastos.domain.filtros;
+
+import java.time.LocalDate;
+import java.util.Objects;
+import umu.tds.gastos.domain.core.Gasto;
+
+public class FiltroFechas implements Filtro {
+    private final LocalDate fechaInicio;
+    private final LocalDate fechaFin;
+
+    public FiltroFechas(LocalDate fechaInicio, LocalDate fechaFin) {
+        
+        if (fechaFin.isBefore(fechaInicio)) {
+            throw new IllegalArgumentException("fechaFin no puede ser antes de fechaInicio");
+        }
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+    }
+
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public LocalDate getFechaFin() {
+        return fechaFin;
+    }
+
+    @Override
+    public boolean cumple(Gasto g) {
+        if (g == null || g.getFecha() == null) {
+            return false;
+        }
+        LocalDate fechaGasto = g.getFecha();
+        return !fechaGasto.isBefore(fechaInicio) && !fechaGasto.isAfter(fechaFin);
+    }
+}
