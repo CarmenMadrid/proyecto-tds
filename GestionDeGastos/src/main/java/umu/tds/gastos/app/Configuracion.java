8accd52e9c6047ea393548fd441f1cfadb817ee8
ppaulaalgar <p.garciaalgar@um.es>
2026-05-22 18:07:51 +0200

    configuracion actualizada

--- GestionDeGastos/src/main/java/umu/tds/gastos/app/Configuracion.java
+++ GestionDeGastos/src/main/java/umu/tds/gastos/app/Configuracion.java
@@ -1,22 +1,37 @@
 package umu.tds.gastos.app;
 
-import umu.tds.gastos.ui.view.SceneManager;
+import umu.tds.gastos.controller.CuentaController;
+import umu.tds.gastos.persistence.CuentaRepository;
+import umu.tds.gastos.persistence.json.CuentaRepositoryJson;
 
 public class Configuracion {
 
     private static Configuracion instancia;
-    private final SceneManager sceneManager = SceneManager.getInstancia();
+
+    private CuentaRepository cuentaRepository;
+    private CuentaController cuentaController;
+
+    private Configuracion() {
+        this.cuentaRepository = new CuentaRepositoryJson();
+        this.cuentaController = new CuentaController(cuentaRepository);
+    }
+
+    public static Configuracion getInstancia() {
+        if (instancia == null) {
+            instancia = new Configuracion();
+        }
+        return instancia;
+    }
 
     static void setInstancia(Configuracion impl) {
-		Configuracion.instancia = impl;
-	}
+        Configuracion.instancia = impl;
+    }
 
-	public static Configuracion getInstancia() {
-		return Configuracion.instancia;
-	}
+    public CuentaController getCuentaController() {
+        return cuentaController;
+    }
 
-	public SceneManager getSceneManager() {
-		return sceneManager;
-	}
-
+    public CuentaRepository getCuentaRepository() {
+        return cuentaRepository;
+    }
 }
